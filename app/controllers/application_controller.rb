791d1e2b1e147a6bf85a6e19326d72dbc2ab4716
Angelo capilleri <capilleri@yahoo.com>
2013-11-25 16:06:53 +0100

    set default layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  layout 'jobs'
   def after_sign_in_path_for(resource)
     cookies[:job].present? ? fine_job_steps_path : root_path
   end
